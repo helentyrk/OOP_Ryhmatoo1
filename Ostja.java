f4794a609bd1e30a55798eb74c34fad6b1d8aa8d
helentyrk <32064727+helentyrk@users.noreply.github.com>
2020-04-01 15:23:13 +0300

    Create Ostja.java

---
+++ Ostja.java
@@ -0,0 +1,25 @@
+//Ostja teeb oma valikud: 1) kontserdi pealkiri, 2) kohtade arv.
+//Siia võib ehk meetoditena panna ka muud valikud..
+
+import java.util.Scanner;
+
+public class Ostja {
+    private String nimi;
+
+    public Ostja(String nimi) {
+        this.nimi = nimi;
+    }
+    public String valiKontsert() {
+        Scanner kontsert = new Scanner(System.in);
+        System.out.println("Sisesta soovitud kontserdi nimi: ");
+        String soovitudKontserdiNimi = kontsert.next();
+        return soovitudKontserdiNimi;
+
+    }
+    public int valiKohtadeArv(){
+        Scanner arv = new Scanner(System.in);
+        System.out.println("Sisesta soovitud kohtade arv: ");
+        int soovitudArv = arv.nextInt();
+        return soovitudArv;
+    }
+}
