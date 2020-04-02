0e431f29100110b8836fabcf5543d43f9db65799
helentyrk <32064727+helentyrk@users.noreply.github.com>
2020-04-02 10:00:55 +0300

    Update Ostja.java

--- Ostja.java
+++ Ostja.java
@@ -1,5 +1,4 @@
 //Ostja teeb oma valikud: 1) kontserdi pealkiri, 2) kohtade arv.
-
 import java.util.Scanner;
 
 public class Ostja {
@@ -8,12 +7,14 @@ public class Ostja {
     public Ostja(String nimi) {
         this.nimi = nimi;
     }
-   public String valiKontsert() {
+    public String getNimi() {
+        return nimi;
+    }
+    public String valiKontsert() {
         Scanner kontsert = new Scanner(System.in);
         System.out.println("Sisesta soovitud kontserdi nimi: ");
         String soovitudKontserdiNimi = kontsert.next();
         return soovitudKontserdiNimi;
-
     }
     public int valiKohtadeArv(){
         Scanner arv = new Scanner(System.in);
