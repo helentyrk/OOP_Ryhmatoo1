bcb7ff23e8daede6be7ca11affaba6189a0eb048
helentyrk <32064727+helentyrk@users.noreply.github.com>
2020-04-02 13:37:54 +0300

    Update Peaklass.java

--- Peaklass.java
+++ Peaklass.java
@@ -3,27 +3,21 @@ import java.util.*;
 public class Peaklass {
 
     public static void main(String[] args) {
-        //Valikus olevad kontserdid
+        //Valikus olevad kontserdid ja nende massiiv ning uus ostja
         Kontsert kontsert1 = new Kontsert("Päike", "Vanemuise kontserdisaal", "02-04-2020", 35, 15);
         Kontsert kontsert2 = new Kontsert("Rahu", "Viljandi Pärimusmuusika Ait", "04-30-2020", 20, 13);
-
-        //Paneme kontserdid massiivi kokku
         Kontsert[] kontserdid = {kontsert1, kontsert2};
-
-        //Ostjad
         Ostja ostja1 = new Ostja("Mari");
 
         //Programmi tutvustav tekst
         System.out.println("Tere tulemast, " + ostja1.getNimi() + "! Selle programmi abil saab osta kontsertide pileteid.");
-        System.out.println("Hetkel on valikus sellised kontserdid: " + kontsert1.getKontserdiPealkiri() + ", " + kontsert2.getKontserdiPealkiri() + ".");
+        System.out.println("Hetkel on valikus sellised kontserdid: " + kontsert1.getKontserdiPealkiri() + " , " + kontsert2.getKontserdiPealkiri() + ".");
 
-        //Ostja valib kontserdi
+        //Ostja valib kontserdi meetodiga 'valiKontsert', esialgu paneme "valitud kontserdi" väärtuseks näiteks esimese kontserdi.
         String valitudPealkiri = ostja1.valiKontsert();
-
-        //Programm vaatab, kas selline kontsert on valikus ja kui on, siis kinnitab valiku.
         Kontsert valitudKontsert = kontsert1;
 
-        //Kogume kontsertide pealkirjad listi, et kontrollida, kas valitud kontsert on olemas.
+        //Programm vaatab, kas selline kontsert on valikus ja kui on, siis kinnitab valiku.
         List<String> pealkirjad = new ArrayList<String>();
 
         for (Kontsert k : kontserdid) {
@@ -34,25 +28,22 @@ public class Peaklass {
             System.out.println("Sellist kontserti valikus ei ole. Palun vali uuesti.");
             valitudPealkiri = ostja1.valiKontsert();
         }
-        if (pealkirjad.contains(valitudPealkiri)){
-            for (Kontsert k : kontserdid){
-                String pealk = k.getKontserdiPealkiri();
-                if (valitudPealkiri.equals(pealk)){
-                    valitudKontsert = k;
+            if (pealkirjad.contains(valitudPealkiri)) {
+                for (Kontsert k : kontserdid) {
+                    String pealk = k.getKontserdiPealkiri();
+                    if (valitudPealkiri.equals(pealk)) {
+                        valitudKontsert = k;
+                    }
                 }
+                System.out.println(valitudKontsert.toString());
             }
-            System.out.println(valitudKontsert.toString());
-        }
-
+        //Vaatan konkreetse konserdi kohtade arvu suurust ja loon vastava massiivi
         int kohtadeArv = valitudKontsert.getMüüdavatePiletiteArv();
-
-        //loon kohtade arvu massiivi, 1 kuni müüdavate piletite koguarv
         int[] kohad = new int[kohtadeArv];
         for (int i = 0; i < kohtadeArv; i++) {
             kohad[i] = i + 1;
         }
-
-        //Soovitud piletite arv
+        //Soovitud piletite arvu küsimine
         int soovitudPiletiteArv = ostja1.valiKohtadeArv();
 
         while (soovitudPiletiteArv > valitudKontsert.getMüüdavatePiletiteArv()){
@@ -60,12 +51,12 @@ public class Peaklass {
             soovitudPiletiteArv = ostja1.valiKohtadeArv();
         }
         System.out.println("Soovitud pileteid: " + soovitudPiletiteArv);
-        
-        //Soovitud kuupäev
+
+        //Soovitud kuupäeva küsimine
         String soovitudKuupäev = ostja1.valiKuupäev();
         System.out.println("Soovitud kuupäev: " + soovitudKuupäev);
 
-        //Loon müüdud kohtade listi
+        //Loon müüdud kohtade listi, et näha, palju kokku on kohti müüdud.
         List<Integer> kohtadeList = new ArrayList<>();
 
         //Kas soovitakse kohta valida või tuleb juhuslikult genereerida?
@@ -83,34 +74,29 @@ public class Peaklass {
                 }
             }
         } else if (valik.equals("ei")) {
-            //genereerib juhuslikud kohad nii, et ei oleks korduvaid kohti.
             List<Integer> juhuslikKoht = new ArrayList<>();
             for (int i = 0; i < kohad.length; i++) {
                 juhuslikKoht.add(i + 1);
             }
-            Collections.shuffle(juhuslikKoht);
-            for (int j = 0; j < soovitudPiletiteArv; j++) {
-                int juhuKohad = juhuslikKoht.get(j);
-                System.out.println("Juhuslikult pakutud koht on: " + juhuKohad);
-                kohtadeList.add(juhuKohad);
-            }
+                Collections.shuffle(juhuslikKoht);
+                for (int j = 0; j < soovitudPiletiteArv; j++) {
+                    int juhuKohad = juhuslikKoht.get(j);
+                    System.out.println("Juhuslikult pakutud koht on: " + juhuKohad);
+                    kohtadeList.add(juhuKohad);
+                }
         }
         //Arvutame müüdud piletite maksumuse, anname ostjale teada ja täname ostu eest.
         Piletimüük piletimüük1 = new Piletimüük(valitudKontsert, valitudKontsert.getPiletiHind());
         int ostetudPiletid = piletimüük1.piletiteMaksumus(valitudKontsert.getPiletiHind(), kohtadeList.size());
         System.out.println("Piletite maksumus on kokku " + ostetudPiletid + " eurot. Täname ostu eest!");
 
-        //Sorteerime võetud kohtade listi.
+        //Teeme kohtade listi String tüüpi muutujateks ja prindime välja hõivatud kohad.
         Collections.sort(kohtadeList);
-
-        //Teeme kohtade listi String tüüpi muutujateks ja lisame välja printimiseks komad kohtade numbrite vahele.
         List<String> kohadStringiks = new ArrayList<>();
-        for(Integer koht : kohtadeList) {
+        for(Integer koht :  kohtadeList) {
             kohadStringiks.add(String.valueOf(koht));
         }
         String võetudKohad = String.join(", ", kohadStringiks);
-
-        // Prindime välja võetud kohtade nimekirja.
         System.out.println("Kontserdil on nüüd hõivatud kohad " + võetudKohad + ".");
     }
 }
