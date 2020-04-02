d35679b8d16019862a53d37f761b47b7f0828f9c
helentyrk <32064727+helentyrk@users.noreply.github.com>
2020-04-02 10:46:11 +0300

    Update Ostja.java

--- Ostja.java
+++ Ostja.java
@@ -1,4 +1,7 @@
-//Ostja teeb oma valikud: 1) kontserdi pealkiri, 2) kohtade arv.
+//Ostja teeb oma valikud: 1) kontserdi pealkiri, 2) kohtade arv, 3) kuupäev.
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Ostja {
@@ -21,5 +24,20 @@ public class Ostja {
         System.out.println("Sisesta soovitud kohtade arv: ");
         int soovitudArv = arv.nextInt();
         return soovitudArv;
+    }
+    public String valiKuupäev(){
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Sisesta kuupäev kujul: päev-kuu-aasta");
+        System.out.print("Sisesta kuupäev: ");
+        String soovitudKuupäev = scan.nextLine();
+        try {
+            Date date = sdf.parse(soovitudKuupäev);
+            sdf = new SimpleDateFormat("EEE, d MMM yyyy");
+            System.out.println("Soovitud kuupäev: " + sdf.format(date));
+        } catch (ParseException e) {
+            System.out.println("Parse Exception");
+        }
+        return soovitudKuupäev;
     }
 }
